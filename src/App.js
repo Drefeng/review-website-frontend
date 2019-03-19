146438d07d349afb5bff866ea3f714cb376a903c
martikbj <martin_bjerkeengen@hotmail.com>
2019-03-19 14:57:45 +0100

    fixed login/register

--- src/App.js
+++ src/App.js
@@ -1,28 +1,124 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { render } from "react-dom";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import Tabs from './Tabs';
+require('./styles.css');
+
+function App() {
+  return (
+    <div>
+      <h1>Login or Register</h1>
+     <Tabs>
+      <div label="Login">
+      <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <form>
+            <p className="h4 text-center mb-4">Sign in</p>
+            <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="defaultFormLoginEmailEx"
+              className="form-control"
+            />
+            <br />
+            <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+              Your password
+            </label>
+            <input
+              type="password"
+              id="defaultFormLoginPasswordEx"
+              className="form-control"
+            />
+            <div className="text-center mt-4">
+              <MDBBtn color="indigo" type="submit">Login</MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
       </div>
-    );
-  }
+      <div label="Register">
+      <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <form>
+            <p className="h4 text-center mb-4">Sign up</p>
+            <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
+              Your name
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="defaultFormRegisterEmailEx"
+              className="form-control"
+            />
+            <br />
+            <label
+              htmlFor="defaultFormRegisterConfirmEx"
+              className="grey-text"
+            >
+              Confirm your email
+            </label>
+            <input
+              type="email"
+              id="defaultFormRegisterConfirmEx"
+              className="form-control"
+            />
+            <br />
+            <label
+              htmlFor="defaultFormRegisterPasswordEx"
+              className="grey-text"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              id="defaultFormRegisterPasswordEx"
+              className="form-control"
+            />
+            <div className="md-form pb-3">
+                <div className="form-check my-4">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="defaultCheck17"
+                  />
+                  <label
+                    className="form-check-label white-text"
+                    htmlFor="defaultCheck17"
+                  >
+                    Check if registering as restaurant owner
+                  </label>
+                </div>
+              </div>
+            <div className="text-center mt-4">
+              <MDBBtn color="unique" type="submit">
+                Register
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+      </div>
+    </Tabs>
+    </div>
+  );
 }
 
+const container = document.createElement('div');
+document.body.appendChild(container);
 export default App;
