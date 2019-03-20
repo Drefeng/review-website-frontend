05fc52e3592d2aa7b0b50a97816d9237e1ef731c
Billy Infante <infbil15@gmail.com>
2019-03-20 12:01:07 +0100

    WeebMaister5000

--- src/App.js
+++ src/App.js
@@ -9,33 +9,30 @@ import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
 import "./index.css";
-import RenderToLayer from "material-ui/internal/RenderToLayer";
 require('./styles.css');
 
 class App extends Component{
 
   
    render(){
-     return (
-      <HashRouter>
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/stuff">Login/Register</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
-        <div className="content">
-          <Route exact path="/" component={Home}/>
-          <Route path="/stuff" component={Stuff}/>
-          <Route path="/contact" component={Contact}/>
-        </div>
-      </div>
-      </HashRouter>
-);
+      return (
+          <HashRouter>
+          <div>
+            <h1>Simple SPA</h1>
+            <ul className="header">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/stuff">Login/Register</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" component={Home}/>
+              <Route path="/stuff" component={Stuff}/>
+              <Route path="/contact" component={Contact}/>
+            </div>
+          </div>
+          </HashRouter>
+      );
    }
-  }
-
-
+}
 
 export default App;
