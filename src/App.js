aa0cac92bd098c702f36c3619e26e7ff25d0f8e7
martikbj <martin_bjerkeengen@hotmail.com>
2019-03-20 09:32:19 +0100

    added subpages and moved login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-// import { render } from "react-dom";
-// import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
-import Login from './Login'
-import Register from './Register'
-
-import Tabs from './Tabs';
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+import "./index.css";
 require('./styles.css');
 
 function App() {
   return (
+    <HashRouter>
     <div>
-      <h1>Login or Register</h1>
-     <Tabs>
-      <div label="Login">
-        <Login />
+      <h1>Simple SPA</h1>
+      <ul className="header">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/stuff">Login/Register</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+      <div className="content">
+        <Route exact path="/" component={Home}/>
+        <Route path="/stuff" component={Stuff}/>
+        <Route path="/contact" component={Contact}/>
       </div>
-      <div label="Register">
-        <Register />
-      </div>
-    </Tabs>
     </div>
-  );
+    </HashRouter>
+);
 }
 
 const container = document.createElement('div');
