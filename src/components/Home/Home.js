0b402132a71f02474f51d3cf4127fcf69092c381
Martin <martin_bjerkeengen@hotmail.com>
2019-03-21 16:38:37 +0100

    added tabs to ownerdash/reviewdash

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
+import RestaurantList from "../Restaurant/RestaurantList/RestaurantList";
+
  
 class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+  constructor(props){
+    super(props);
+    this.state = {
+      restaurants: [],
+  };
+}
+
+/* componentWillMount(){
+
+  fetch('https://review-website-api.herokuapp.com/restaurant')
+  .then(data => data.json())
+      .then((data) => { this.setState({ restaurants: data }) 
+    }); 
+
+} */
+
+
+render() {
+  return (
+    <div>
+        Test
       </div>
     );
   }
