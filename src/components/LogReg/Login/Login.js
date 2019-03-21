3ef6c89b3fa90c320feab7214c3cddf616c8d568
fajardoralf <fajardoralf@gmail.com>
2019-03-21 14:58:55 +0100

    added create json

--- src/components/LogReg/Login/Login.js
+++ src/components/LogReg/Login/Login.js
@@ -1,21 +1,63 @@
 import React, {Component} from 'react';
-import { render } from "react-dom";
 
 
-class Login extends React.Component{
+class Login extends Component{
+    constructor(){
+        super();
+        this.state = {
+            username: "",
+            password:""
+        }
+    }
+
+    handleInputChange = (event) => {
+        const {value, name} = event.target;
+        this.setState({
+            [name] : value,
+        });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        fetch('https://review-website-api.herokuapp.com/user/login', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res)=> {
+            if(res.status === 200) {
+                return res.json();
+            }else {
+                const error = new Error(res.error);
+                throw error;
+            }
+        })
+        .then(data =>{
+            console.log(data);
+            localStorage.setItem('token',data.accessToken);
+        })
+        .catch(err => {
+            console.log(err);
+            alert('error logging in please try again');
+        })
+    }
+
     render() {
         return (
-          <form action="/login" method="post">
+          <form  method="post">
           <div>
               <label>Username:</label>
-              <input type="text" name="username"/>
+              <input type="text" onChange={this.handleInputChange} name="username"/>
           </div>
           <div>
               <label>Password:</label>
-              <input type="password" name="password"/>
+              <input type="password" onChange={this.handleInputChange} name="password"/>
           </div>
           <div>
-              <input type="submit" value="Log In"/>
+              <input type="submit"  onClick={this.onSubmit} value="Log In"/>
           </div>
       </form>
         );
