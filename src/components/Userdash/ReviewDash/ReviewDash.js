649cb04ff341bbc52948b71b1a53241aaac4e085
Martin <martin_bjerkeengen@hotmail.com>
2019-03-22 15:48:17 +0100

    fetch users reviews

--- src/components/Userdash/ReviewDash/ReviewDash.js
+++ src/components/Userdash/ReviewDash/ReviewDash.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
 import Tabs from "../../Tabs/Tabs";
+import RecentCards from "../../cardsItems/recentReviewsCard";
 class ReviewDash extends Component{
     
+    constructor(){
+        super()
+        this.state = {
+            yourReviews: [],
+        };
+
+    }
+
+    getRecentReview(){
+        fetch('/review/user/' + localStorage.getItem('id')).then(res => res.json())
+        .then(data => {
+            this.setState({
+                yourReviews: data.rows
+            })
+        }).catch(err => {
+          console.log(err);
+    });
+      }
+
     render(){
+
+        const reviews = this.state.yourReviews.map(item => (
+            <RecentCards
+               key={item.review_id}
+               name={item.name}
+     description={item.review_text}
+     rating={item.rating}
+     updated={item.updated_at}
+ />
+        ));
         return(
             <Tabs>
-                <div label ='List your reviews' />
-                
-                <div label ='Create new review' />
+                <div label ='List your reviews'>
+                    {reviews}
+  ));
+                </div>
+                <div label ='Create new review'>
+
+                </div>
             </Tabs>
         );
     }
